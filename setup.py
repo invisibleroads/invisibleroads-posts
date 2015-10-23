04ef3e0bafb348bcf509465ebedbd4b835c073cc
Roy Hyunjin Han <rhh@crosscompute.com>
2015-10-23 00:55:38 -0400

    Fix PyPI classifier

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     long_description=DESCRIPTION,
     classifiers=[
         'Programming Language :: Python',
-        'Framework :: Pyramid :: InvisibleRoads',
+        'Framework :: Pyramid',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
