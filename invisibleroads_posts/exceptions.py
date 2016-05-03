53c2fe2092aaf94ed97f51ae43b64a59c40ed7c1
Roy Hyunjin Han <rhh@crosscompute.com>
2016-05-03 04:12:11 -0400

    Fix content_type

--- invisibleroads_posts/exceptions.py
+++ invisibleroads_posts/exceptions.py
@@ -3,11 +3,11 @@ from pyramid.httpexceptions import HTTPBadRequest, HTTPNotFound
 
 
 class HTTPErrorJSONMixin(object):
-    content_type = 'application/json'
 
     def __init__(self, value_by_key):
         super(HTTPErrorJSONMixin, self).__init__()
         self.body = json.dumps(value_by_key)
+        self.content_type = 'application/json'
 
 
 class HTTPNotFoundJSON(HTTPErrorJSONMixin, HTTPNotFound):
