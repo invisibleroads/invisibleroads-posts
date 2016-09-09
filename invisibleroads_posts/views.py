ed067615155e7936050773f635916d10a120ea56
Roy Hyunjin Han <rhh@crosscompute.com>
2016-09-09 08:45:08 -0400

    Rename is_bad_request to reject_bad_request

--- invisibleroads_posts/views.py
+++ invisibleroads_posts/views.py
@@ -8,7 +8,7 @@ def add_routes(config):
     config.add_view(
         renderer='invisibleroads_posts:templates/posts.jinja2',
         route_name='index')
-    config.add_view(is_bad_request, context=HTTPBadRequest)
+    config.add_view(reject_bad_request, context=HTTPBadRequest)
 
 
 def expect_param(key, params):
@@ -18,7 +18,7 @@ def expect_param(key, params):
         raise HTTPBadRequest({key: 'required'})
 
 
-def is_bad_request(context, request):
+def reject_bad_request(context, request):
     response = request.response
     response.body = json.dumps(context.detail).encode('utf-8')
     response.status_int = context.status_int
