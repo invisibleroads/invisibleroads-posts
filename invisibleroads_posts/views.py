0dc83666ca4007db42c6093c7599a4ae399c2ea9
Roy Hyunjin Han <rhh@crosscompute.com>
2017-01-07 21:07:05 -0500

    Remove underscore from error message

--- invisibleroads_posts/views.py
+++ invisibleroads_posts/views.py
@@ -31,11 +31,11 @@ def expect_param(key, params, parse=None, message=''):
 
 
 def expect_integer(key, params, minimum_value=None, maximum_value=None):
-    value = expect_param(key, params, int, 'expected_integer')
+    value = expect_param(key, params, int, 'expected integer')
     if minimum_value and value < minimum_value:
-        raise HTTPBadRequest({key: 'expected_integer >= %s' % minimum_value})
+        raise HTTPBadRequest({key: 'expected integer >= %s' % minimum_value})
     if maximum_value and value > maximum_value:
-        raise HTTPBadRequest({key: 'expected_integer <= %s' % maximum_value})
+        raise HTTPBadRequest({key: 'expected integer <= %s' % maximum_value})
     return value
 
 
