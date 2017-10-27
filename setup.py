ad5826f6f6769fa1d22e72528dd71ef7e4521cf3
Roy <rhh@crosscompute.com>
2017-10-27 17:09:42 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads-posts',
-    version='0.5.6',
+    version='0.5.6.1',
     description='Web application defaults',
     long_description=DESCRIPTION,
     classifiers=[
