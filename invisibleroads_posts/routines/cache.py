c0acd7c566c249455f620969051f3ca95ebf6bf8
Roy Hyunjin Han <rhh@crosscompute.com>
2020-05-04 23:38:11 -0400

    Revert to bootstrapped settings

--- invisibleroads_posts/routines/cache.py
+++ invisibleroads_posts/routines/cache.py
@@ -1,4 +1,5 @@
 def configure_cache(settings, prefix, cache):
     if prefix + 'backend' not in settings:
         settings[prefix + 'backend'] = 'dogpile.cache.memory'
+    settings[prefix + 'replace_existing_backend'] = True
     cache.configure_from_config(settings, prefix)
