fcf299124ed97899b0f05eb5ceeececf64a7b729
Roy Hyunjin Han <rhh@crosscompute.com>
2020-07-31 00:51:12 -0400

    Add cors preflight predicate thanks to @mmerickel https://gist.github.com/mmerickel/1afaf64154b335b596e4

--- invisibleroads_posts/__init__.py
+++ invisibleroads_posts/__init__.py
@@ -1,13 +1,38 @@
 from invisibleroads_macros_configuration import set_default
 from os import environ
+from pyramid.events import NewResponse
+from pyramid.security import NO_PERMISSION_REQUIRED
+from pyramid.settings import aslist
 
 from .routines.cache import configure_cache
 from .routines.configuration import fill_settings
 from .variables import FUNCTION_CACHE
 
 
+class CorsPreflightPredicate(object):
+
+    def __init__(self, value, config):
+        self.value = value
+
+    def text(self):
+        return 'cors_preflight = %s' % self.value
+
+    phash = text
+
+    def __call__(self, context, request):
+        if not self.value:
+            return False
+        method = request.method
+        headers = request.headers
+        return (
+            'OPTIONS' == method and
+            'Origin' in headers and
+            'Access-Control-Request-Method' in headers)
+
+
 def includeme(config):
     configure_settings(config)
+    configure_cross_origin_resource_sharing(config)
     configure_caches(config)
 
 
@@ -19,10 +44,49 @@ def configure_settings(config):
         config.add_request_method(
             lambda request: settings['data.folder'], 'data_folder', reify=True)
 
-    for line in settings.get(
-            'application.environment', '').strip().splitlines():
+    for line in aslist(settings.get('application.environment', '')):
         k, v = line.split()
         environ[k] = v
+
+
+def configure_cross_origin_resource_sharing(config):
+    settings = config.get_settings()
+    allowed_origins = aslist(settings.get('cors.allowed_origins', '').lower())
+    if not allowed_origins:
+        return
+    allowed_methods = aslist(settings.get('cors.allowed_methods', '').upper())
+    allowed_headers = aslist(settings.get('cors.allowed_headers', '').title())
+
+    def see_options(request):
+        response = request.response
+        response_headers = response.headers
+        if 'Access-Control-Allow-Origin' not in response_headers:
+            return response
+        response_headers['Access-Control-Allow-Methods'] = ','.join(
+            allowed_methods)
+        response_headers['Access-Control-Allow-Headers'] = ','.join(
+            allowed_headers)
+        return response
+
+    def set_cross_origin_resource_sharing(event):
+        request = event.request
+        request_headers = request.headers
+        origin = request_headers.get('Origin')
+        if origin.lower() not in allowed_origins:
+            return
+        response = event.response
+        response_headers = response.headers
+        response_headers['Access-Control-Allow-Origin'] = origin
+        if len(allowed_origins) > 1:
+            response_headers['Vary'] = 'Origin'
+
+    # https://gist.github.com/mmerickel/1afaf64154b335b596e4#file-cors-py-L39
+    config.add_route_predicate('cors_preflight', CorsPreflightPredicate)
+    config.add_route('corsPreflight', '/{all:.*}', cors_preflight=True)
+    config.add_view(
+        see_options, route_name='corsPreflight',
+        permission=NO_PERMISSION_REQUIRED)
+    config.add_subscriber(set_cross_origin_resource_sharing, NewResponse)
 
 
 def configure_caches(config):
