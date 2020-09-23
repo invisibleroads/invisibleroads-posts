537ca0d7eddc5e2a3c1c36f119dcc37100005698
Roy Hyunjin Han <rhh@crosscompute.com>
2020-09-23 13:25:43 +0000

    Set Access-Control-Allow-Credentials

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
 
 setup(
     name='invisibleroads-posts',
-    version='0.7.15',
+    version='0.7.15.1',
     description='Web application defaults',
     long_description=DESCRIPTION,
     long_description_content_type='text/markdown',
